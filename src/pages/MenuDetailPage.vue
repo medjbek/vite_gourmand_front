78ea56501f3fe6162b0ab1269967fab217b42ee1
medjbek <medjbek@gmail.com>
2026-02-08 15:45:29 +0100

    VG24 preparation deploiement front ( VITE_API_URL + .env ignore)

--- src/pages/MenuDetailPage.vue
+++ src/pages/MenuDetailPage.vue
@@ -196,7 +196,7 @@ async function loadMenuById(menuId) {
     fetchController = new AbortController()
   }
 
-  const API_URL = 'http://127.0.0.1:8000/api'
+  const API_URL = import.meta.env.VITE_API_URL
 
   const res = await fetch(`${API_URL}/menus/${menuId}`, {
     signal: fetchController.signal,
